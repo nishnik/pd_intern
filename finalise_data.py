b4053041cc48347e7511ff57163d3958fe444fad
Nishant Nikhil <i.nishantnikhil@gmail.com>
2016-12-21 17:55:44 +0530

    Update finalise_data.py

--- finalise_data.py
+++ finalise_data.py
@@ -15,6 +15,26 @@ for a in list(pubmed_fetch.keys()):
     except:
         print (a)
 
+import re
+f = open('stopwords.txt', 'r')
+stopwords = f.read()
+list_stopwords = re.sub("[^\w]", " ",  stopwords).split()
+
+# Remove punctuation and remove special chars and lower it
+import string
+regex = re.compile('[%s]' % re.escape(string.punctuation))
+for a in pubmed_fetch:
+    temp = regex.sub('', pubmed_fetch[a]['abstract'])
+    temp = temp.lower()
+    pubmed_fetch[a]['abstract'] = "".join([c for c in temp if c in string.ascii_lowercase or c in string.whitespace])
+
+done = 0
+for a in list(pubmed_fetch.keys()):
+    print (done)
+    done += 1
+    words = re.sub("[^\w]", " ",  pubmed_fetch[a]['abstract']).split()
+    pubmed_fetch[a]['abstract'] = ' '.join(word for word in words  if word not in list_stopwords)
+
 to_vanish =  set(connection.keys()) - set(pubmed_fetch.keys())
 
 for a in to_vanish:
@@ -30,14 +50,7 @@ for a in connection:
                 print (a, b)
                 pass
 
-# Remove punctuation and remove special chars and lower it
-import re
-import string
-regex = re.compile('[%s]' % re.escape(string.punctuation))
-for a in pubmed_fetch:
-    temp = regex.sub('', pubmed_fetch[a]['abstract'])
-    temp = temp.lower()
-    pubmed_fetch[a]['abstract'] = "".join([c for c in temp if c in string.ascii_lowercase or c in string.whitespace or c in string.digits])
+
 
 json.dump(pubmed_fetch, open('pubmed_fetch.json', 'w'))
 json.dump(connection, open('connection_re.json', 'w'))
